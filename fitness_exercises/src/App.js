7c642f5517e4372627ce2f4684992c9cb450bf49
https://gitlab.com/Moatez_Dh <d.moatez.recinov@gmail.com>
2024-02-12 23:24:27 +0100

    first Commit

--- fitness_exercises/src/App.js
+++ fitness_exercises/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import {Box} from '@mui/material'
+import NavBar from './components/NavBar'
+import Home from './components/pages/Home.js'
+import ExerciseDetails from './components/pages/ExerciseDetails.js'
+import Footer from './components/Footer.js'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+        <Box width={"400px"} sx={{width:{xl:"1488px"}}}>
+           <NavBar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/exercise/:id' element={<ExerciseDetails/>}/>
 
-export default App;
+            </Routes>
+            <Footer />
+        </Box>
+    </div>
+  )
+}
