931a0e840c520a033f24dc412347cc52f853dda6
https://gitlab.com/Moatez_Dh <d.moatez.recinov@gmail.com>
2024-02-22 14:11:32 +0100

    last update

--- fitness_exercises/src/App.js
+++ fitness_exercises/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
 import {Box} from '@mui/material'
@@ -6,9 +6,22 @@ import NavBar from './components/NavBar'
 import Home from './pages/Home.js'
 import ExerciseDetails from './pages/ExerciseDetails.js'
 import Footer from './components/Footer.js'
+
+import Logo from './assets/images/Logo.png'
+
 export default function App() {
+  console.log(Logo);
+  useEffect(() => {
+    document.title = 'Locomotive Gym';
+    const icon = document.createElement('link');
+    icon.rel = 'icon';
+    icon.type = 'image/png';
+    icon.href = {Logo};
+    document.head.appendChild(icon);
+  },[])
   return (
     <div>
+     
         <Box width={"400px"} sx={{width:{xl:"1488px"}}}>
            <NavBar/>
             <Routes>
